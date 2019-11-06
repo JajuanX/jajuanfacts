efd9f8f4cc1addff1c04d6a12e8731363e5e8b8e
JajuanX <jajuan.burton@live.com>
2019-11-06 14:53:00 -0500

    Attempting to deploy

--- src/app/app.js
+++ src/app/app.js
@@ -1,25 +1,52 @@
-import angular from 'angular';
+import angular from 'angular'
 
-import '../style/app.css';
+(function() {
+  'use strict';
 
-let app = () => {
-  return {
-    template: require('./app.html'),
-    controller: 'AppCtrl',
-    controllerAs: 'app'
+  angular.module('app', []).component('facts', {
+      template: require('./app.html'),
+      controller: FactsController,
+      styleUrls: require('../style/app.css'),
+  });
+
+/** @ngInject */
+
+function FactsController($http, $filter) {
+  const $ctrl = this
+
+  $ctrl.$onInit = function() {
+    $ctrl.firstName = "Jajuan"
+    $ctrl.lastName = "X"
+    $ctrl.savedFacts = []
+    $ctrl.key = 0
+    $ctrl.newFact()
   }
-};
 
-class AppCtrl {
-  constructor() {
-    this.url = 'https://github.com/preboot/angular-webpack';
+  $ctrl.newFact = () => {
+    console.log('running');
+    $http.get('https://api.chucknorris.io/jokes/random')
+      .then((data)=> {
+        $ctrl.fact = data.data.value.replace(/Chuck/gi, $ctrl.firstName).replace(/Norris/gi, $ctrl.lastName).replace(/roundhouse/gi, "boot").replace(/kick/gi, "")
+      })
+      .catch((err) => {
+        console.log(err)
+      }
+    )
   }
+
+  $ctrl.saveFact = () => {
+    if (($ctrl.savedFacts.length === 0)) {
+     return $ctrl.savedFacts.push({ fact: $ctrl.fact, key: 0})
+   } else if ($ctrl.fact !== $ctrl.savedFacts[$ctrl.savedFacts.length - 1].fact) {
+        $ctrl.savedFacts.push({ fact: $ctrl.fact, key: ++$ctrl.key })
+     };
+    console.log($ctrl.savedFacts);
+  }
+
+
 }
+})();
 
-const MODULE_NAME = 'app';
-
-angular.module(MODULE_NAME, [])
-  .directive('app', app)
-  .controller('AppCtrl', AppCtrl);
-
-export default MODULE_NAME;
+if (typeof module !== 'undefined' && typeof exports !== 'undefined' && module.exports === exports) {
+  module.exports = 'FactsAboutMe'
+}
